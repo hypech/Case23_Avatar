83b24b74dafeaa857dda0e13156ab65a6ff98b7c
HYPEch.com <55422213+hypech@users.noreply.github.com>
2021-07-20 08:04:25 -0400

    Case23

--- app/src/main/java/com/hypech/case23_avatar/MainActivity.java
+++ app/src/main/java/com/hypech/case23_avatar/MainActivity.java
@@ -1,14 +1,110 @@
 package com.hypech.case23_avatar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_GET = 0;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_SMALL_IMAGE_CUTTING = 2;
+    private static final int REQUEST_BIG_IMAGE_CUTTING = 3;
+    private static final String IMAGE_FILE_NAME = "case23avatar.jpg";
+
+    SharedPreferences mySP;
+    SharedPreferences.Editor myEditor;
+
+    PopupWindowMultipeChoice mPopup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mySP = getSharedPreferences("com.hypech.case23", 0);
+        myEditor = mySP.edit();
     }
+
+    public void click_change_avatar(View v){
+        mPopup = new PopupWindowMultipeChoice(MainActivity.this,
+                                                new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // request permission for gallery
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
+                } else {
+                    mPopup.dismiss();
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType("image/*");
+                    // 判断系统中是否有处理该 Intent 的 Activity
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivityForResult(intent, REQUEST_IMAGE_GET);
+                    } else {
+                        Toast.makeText(MainActivity.this, "No app could open gallery.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // request permission for camera
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                        || ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 300);
+                } else {
+                    mPopup.dismiss();
+                    imageCapture();
+                }
+            }
+        });
+        View rootView = LayoutInflater.from(MainActivity.this)
+                .inflate(R.layout.activity_main, null);
+        mPopup.showAtLocation(rootView,
+                Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
+    }
+
+    /**
+     * Camera
+     */
+    private void imageCapture() {
+        File pictureFile = new File(Environment.getExternalStorageDirectory(), IMAGE_FILE_NAME);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        Uri pictureUri = FileProvider.getUriForFile(this,
+                "hypech.com.fileProvider", pictureFile);
+        myEditor.putString("imageURI", pictureUri.toString()); //<-- toString()
+        myEditor.commit();
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+
+
 }
