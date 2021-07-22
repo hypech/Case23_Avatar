24f9b341aec60718982329577f1cf4b3cc043a85
HYPEch.com <55422213+hypech@users.noreply.github.com>
2021-07-21 22:01:06 -0400

    done

--- app/src/main/java/com/hypech/case23_avatar/MainActivity.java
+++ app/src/main/java/com/hypech/case23_avatar/MainActivity.java
@@ -1,27 +1,40 @@
 package com.hypech.case23_avatar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = "===> ";
 
+    PopupWindow myPop;
     private static final int REQUEST_IMAGE_GET = 0;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int REQUEST_SMALL_IMAGE_CUTTING = 2;
@@ -31,7 +44,8 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences mySP;
     SharedPreferences.Editor myEditor;
 
-    MyPopup mPopup;
+    Uri mImageUri;
+    View mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,49 +57,132 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void click_change_avatar(View v){
-        mPopup = new MyPopup(MainActivity.this,
-                                                new View.OnClickListener() {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mView = inflater.inflate(R.layout.popup_window, null);
+
+        // create the popup window
+        int width = WindowManager.LayoutParams.MATCH_PARENT;
+        int height = WindowManager.LayoutParams.MATCH_PARENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+
+        myPop = new PopupWindow(mView, width, height, focusable);
+
+        myPop.setAnimationStyle(R.style.popwindow_anim_style);
+        ColorDrawable dw = new ColorDrawable(0x0000000);
+        myPop.setBackgroundDrawable(dw);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        myPop.showAtLocation(mView, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        mView.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                // request permission for gallery
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
-                } else {
-                    mPopup.dismiss();
+            public boolean onTouch(View v, MotionEvent event) {
+                myPop.dismiss();
+                return true;
+            }
+        });
+    }
+
+    public void click_picture(View v){
+        // request permission for gallery
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
+        } else {
+            myPop.dismiss();
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+
+            // find the app to handle image
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(intent, REQUEST_IMAGE_GET);
+            } else {
+                Toast.makeText(MainActivity.this, "No App could open gallery.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void click_camera(View v){
+        // request permission for camera
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 300);
+        } else {
+            myPop.dismiss();
+            imageCapture();
+        }
+    }
+
+    public void click_cancel(View v){
+        myPop.dismiss();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 回调成功
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                // 小图切割
+                // 相册选取
+                case REQUEST_IMAGE_GET:
+                    try {
+                        // startSmallPhotoZoom(data.getData());
+                        startBigPhotoZoom(data.getData());
+                    } catch (NullPointerException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                // 拍照
+                case REQUEST_IMAGE_CAPTURE:
+                    File temp = new File(Environment.getExternalStorageDirectory() + "/" + IMAGE_FILE_NAME);
+                    // startSmallPhotoZoom(Uri.fromFile(temp));
+                    // startBigPhotoZoom(temp);
+            }
+        }
+    }
+
+    /**
+     * 处理权限回调结果
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 200:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    myPop.dismiss();
                     Intent intent = new Intent(Intent.ACTION_PICK);
                     intent.setType("image/*");
+                    startActivityForResult(intent, REQUEST_IMAGE_GET);
                     // 判断系统中是否有处理该 Intent 的 Activity
                     if (intent.resolveActivity(getPackageManager()) != null) {
                         startActivityForResult(intent, REQUEST_IMAGE_GET);
                     } else {
-                        Toast.makeText(MainActivity.this, "No app could open gallery.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "未找到图片查看器", Toast.LENGTH_SHORT).show();
                     }
-                }
-            }
-        }, new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // request permission for camera
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
-                        || ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 300);
                 } else {
-                    mPopup.dismiss();
-                    imageCapture();
+                    myPop.dismiss();
                 }
-            }
-        });
-        View rootView = LayoutInflater.from(MainActivity.this)
-                .inflate(R.layout.activity_main, null);
-        mPopup.showAtLocation(rootView,
-                Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
+                break;
+            case 300:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    myPop.dismiss();
+                    imageCapture();
+                } else {
+                    myPop.dismiss();
+                }
+                break;
+        }
     }
 
     /**
@@ -104,6 +201,107 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
     }
 
+    public void startBigPhotoZoom(File inputFile) {
+        // 创建大图文件夹
+        Uri imageUri = null;
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            String storage = Environment.getExternalStorageDirectory().getPath();
+            File dirFile = new File(storage + "/bigIcon");
+            if (!dirFile.exists()) {
+                if (!dirFile.mkdirs()) {
+                    Log.e("TAG", "文件夹创建失败");
+                } else {
+                    Log.e("TAG", "文件夹创建成功");
+                }
+            }
+            File file = new File(dirFile, System.currentTimeMillis() + ".jpg");
+            imageUri = Uri.fromFile(file);
+            mImageUri = imageUri; // 将 uri 传出，方便设置到视图中
+
+            myEditor.putString("imageURI", imageUri.toString()); //<-- toString()
+            myEditor.commit();
+        }
+
+        // 开始切割
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(getImageContentUri(MainActivity.this, inputFile), "image/*");
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1); // 裁剪框比例
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 600); // 输出图片大小
+        intent.putExtra("outputY", 600);
+        intent.putExtra("scale", true);
+        intent.putExtra("return-data", false); // 不直接返回数据
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri); // 返回一个文件
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        // intent.putExtra("noFaceDetection", true); // no face detection
+        startActivityForResult(intent, REQUEST_BIG_IMAGE_CUTTING);
+    }
+
+    public void startBigPhotoZoom(Uri uri) {
+        // 创建大图文件夹
+        Uri imageUri = null;
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            String storage = Environment.getExternalStorageDirectory().getPath();
+            File dirFile = new File(storage + "/bigIcon");
+            if (!dirFile.exists()) {
+                if (!dirFile.mkdirs()) {
+                    Log.e("TAG", "文件夹创建失败");
+                } else {
+                    Log.e("TAG", "文件夹创建成功");
+                }
+            }
+            File file = new File(dirFile, System.currentTimeMillis() + ".jpg");
+            imageUri = Uri.fromFile(file);
+            mImageUri = imageUri; // 将 uri 传出，方便设置到视图中
+
+            myEditor.putString("imageURI", imageUri.toString()); //<-- toString()
+            myEditor.commit();
+
+        }
+
+        // 开始切割
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1); // 裁剪框比例
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 600); // 输出图片大小
+        intent.putExtra("outputY", 600);
+        intent.putExtra("scale", true);
+        intent.putExtra("return-data", false); // 不直接返回数据
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri); // 返回一个文件
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        // intent.putExtra("noFaceDetection", true); // no face detection
+        startActivityForResult(intent, REQUEST_BIG_IMAGE_CUTTING);
+    }
+
+    public Uri getImageContentUri(Context context, File imageFile) {
+        String filePath = imageFile.getAbsolutePath();
+        Cursor cursor = context.getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new String[] { MediaStore.Images.Media._ID },
+                MediaStore.Images.Media.DATA + "=? ",
+                new String[] { filePath }, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            int id = cursor.getInt(cursor
+                    .getColumnIndex(MediaStore.MediaColumns._ID));
+            Uri baseUri = Uri.parse("content://media/external/images/media");
+            return Uri.withAppendedPath(baseUri, "" + id);
+        } else {
+            if (imageFile.exists()) {
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.DATA, filePath);
+                return context.getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            } else {
+                return null;
+            }
+        }
+    }
 
 
 }
